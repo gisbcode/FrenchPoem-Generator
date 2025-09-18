bef7dfe38f786cd25c7d9edc7119c24237ba056e
Gigi <creative.giovannasb@gmail.com>
2025-09-18 21:35:28 +0100

    Added typewriting in javascript

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "À Cupidon - Ronsard",
+    autoStart: true,
+    cursor: null,
+  });
+}
+
+let poemFormElement = document.getElementById("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
